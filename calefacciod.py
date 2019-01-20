68355c1cc3504e550cfd73d43ae10286312ecd3c
jordiprats <jordi.prats@gmail.com>
2019-01-20 15:49:32 +0100

    mostra estat

--- calefacciod.py
+++ calefacciod.py
@@ -6,12 +6,12 @@ import datetime, time
 timeformat = '%Y-%m-%d %H:%M:%S'
 
 def start_calefaccio():
-    print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" START"
     calefaccio.on()
+    print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" START: "+GPIO.input(26)
 
 def stop_calefaccio():
-    print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" STOP"
     calefaccio.off()
+    print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" STOP: "+GPIO.input(26)
 
 schedule.every().day.at("00:00").do(stop_calefaccio)
 schedule.every().day.at("04:00").do(start_calefaccio)
