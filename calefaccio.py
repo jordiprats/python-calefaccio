a012c6bc0ed4fdb86d999cc43d51104ceea11d95
jordiprats <jordi.prats@gmail.com>
2019-01-20 15:36:07 +0100

    rework

--- calefaccio.py
+++ calefaccio.py
@@ -1,14 +1,13 @@
 import RPi.GPIO as GPIO
 
-class Calefaccio:
-    def __init__(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(26, GPIO.OUT)
-        GPIO.output(26, GPIO.LOW)
+def init():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(26, GPIO.OUT)
+    GPIO.output(26, GPIO.LOW)
 
-    def on():
-        GPIO.output(26, GPIO.LOW)
+def on():
+    GPIO.output(26, GPIO.LOW)
 
-    def off():
-        GPIO.output(26, GPIO.HIGH)
+def off():
+    GPIO.output(26, GPIO.HIGH)
--- calefacciod.py
+++ calefacciod.py
@@ -4,22 +4,21 @@ import calefaccio
 import datetime, time
 
 timeformat = '%Y%-m-%d %H:%M:%S'
-global_calefaccio = calefaccio.Calefaccio()
 
 def start_calefaccio():
-    global calefaccio
     print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" START"
-    global_calefaccio.on()
+    calefaccio.on()
 
 def stop_calefaccio():
-    global calefaccio
     print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" STOP"
-    global_calefaccio.off()
+    calefaccio.off()
 
 schedule.every().day.at("00:00").do(stop_calefaccio)
 schedule.every().day.at("04:00").do(start_calefaccio)
 # demo
 schedule.every(1).minutes.do(stop_calefaccio)
+
+calefaccio.init()
 
 while True:
     schedule.run_pending()
