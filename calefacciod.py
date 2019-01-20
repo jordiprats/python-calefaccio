e9e3e8839e8706e7fa8252620578bb3da51f8087
jordiprats <jordi.prats@gmail.com>
2019-01-20 13:45:17 +0100

    output visible

--- calefacciod.py
+++ calefacciod.py
@@ -1,13 +1,17 @@
 import schedule
 import time
 import calefaccio
+import datetime, time
 
+timeformat = '%Y%-m-%d %H:%M:%S'
 calefaccio = calefaccio.Calefaccio()
 
 def start_calefaccio():
+    print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" START"
     calefaccio.on()
 
 def stop_calefaccio():
+    print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" STOP"
     calefaccio.off()
 
 schedule.every().day.at("00:00").do(stop_calefaccio)
