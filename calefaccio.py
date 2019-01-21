96777320b3f6262da9daab9ec859872f3693aed9
jordiprats <jordi.prats@gmail.com>
2019-01-21 18:12:38 +0100

    sample mode

--- calefaccio.py
+++ calefaccio.py
@@ -1,19 +1,30 @@
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+    sample_mode=False
+except:
+    sample_mode=True
 
 def init():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(26, GPIO.OUT)
-    GPIO.output(26, GPIO.LOW)
+    if not sample_mode:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(26, GPIO.OUT)
+        GPIO.output(26, GPIO.LOW)
 
 def on():
-    GPIO.output(26, GPIO.LOW)
+    if not sample_mode:
+        GPIO.output(26, GPIO.LOW)
 
 def off():
+    if not sample_mode:
     GPIO.output(26, GPIO.HIGH)
 
 def status():
-    if GPIO.input(26):
+    if sample_mode:
+        status_bool=sample_mode
+    else:
+        status_bool=GPIO.input(26)
+    if status_bool:
         return "off"
     else:
         return "on"
