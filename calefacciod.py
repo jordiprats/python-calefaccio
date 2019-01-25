c7a74670868133b39284783a3beae7fd437f0b0f
jordiprats <jordi.prats@gmail.com>
2019-01-25 21:45:49 +0100

    threaded scheduler

--- calefacciod.py
+++ calefacciod.py
@@ -3,6 +3,8 @@ import time
 import calefaccio
 import datetime, time
 
+from threading import Thread
+
 timeformat = '%Y-%m-%d %H:%M:%S'
 
 def start_calefaccio():
@@ -13,6 +15,11 @@ def stop_calefaccio():
     calefaccio.off()
     print datetime.datetime.fromtimestamp(time.time()).strftime(timeformat)+" set to "+calefaccio.status()
 
+def run_scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 schedule.every().day.at("00:00").do(stop_calefaccio)
 schedule.every().day.at("04:00").do(start_calefaccio)
 
@@ -20,6 +27,7 @@ calefaccio.init()
 time.sleep(1)
 start_calefaccio()
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+scheduler_thread = Thread(target = run_scheduler, args = ())
+scheduler_thread.start()
+
+scheduler_thread.join()
