d3ef6b2ddb1f35f333ab1fb93be0c8fc5c237b49
jordiprats <jordi.prats@gmail.com>
2019-01-25 22:29:14 +0100

    skel telegram integration

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ echo out > /sys/class/gpio/gpio26/direction
 sleep 1
 echo 0 > /sys/class/gpio/gpio26/value
 ```
+## calefacciod.config
+
+```
+[bot]
+
+token = TOKEN_HERE
+
+[schedule]
+
+daily_start = "05:00"
+daily_stop = "00:00"
+```
--- calefacciod.py
+++ calefacciod.py
@@ -1,9 +1,14 @@
-import schedule
+import sys
 import time
+import logging
+import schedule
+import telegram
 import calefaccio
 import datetime, time
 
 from threading import Thread
+from telegram.ext import Updater, CommandHandler
+from ConfigParser import SafeConfigParser
 
 timeformat = '%Y-%m-%d %H:%M:%S'
 
@@ -20,14 +25,51 @@ def run_scheduler():
         schedule.run_pending()
         time.sleep(1)
 
-schedule.every().day.at("00:00").do(stop_calefaccio)
-schedule.every().day.at("04:00").do(start_calefaccio)
+def telegram_start():
+    user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
+    update.message.reply_text("DEMO START "+user_id+" "+chat_id, use_aliases=True)
 
-calefaccio.init()
-time.sleep(1)
-start_calefaccio()
+def telegram_show_status():
+    update.message.reply_text("DEMO STATUS", use_aliases=True)
 
-scheduler_thread = Thread(target = run_scheduler, args = ())
-scheduler_thread.start()
 
-scheduler_thread.join()
+BOT_TOKEN = ""
+
+# main
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    try:
+        basedir = sys.argv[1]
+    except IndexError:
+        basedir = '.'
+
+    config = SafeConfigParser()
+    config.read(basedir+'/calefacciod.config')
+
+    BOT_TOKEN = config.get('bot', 'token').strip('"').strip("'").strip()
+
+    schedule.every().day.at(config.get('schedule', 'daily_stop').strip('"').strip("'").strip()).do(stop_calefaccio)
+    schedule.every().day.at(config.get('schedule', 'daily_start').strip('"').strip("'").strip()).do(start_calefaccio)
+
+    calefaccio.init()
+    time.sleep(1)
+    start_calefaccio()
+
+    scheduler_thread = Thread(target = run_scheduler, args = ())
+    scheduler_thread.start()
+
+    updater = Updater(token=BOT_TOKEN)
+
+    dp = updater.dispatcher
+
+    updater.dispatcher.add_handler(CommandHandler('start', telegram_start))
+    updater.dispatcher.add_handler(CommandHandler('status', telegram_show_status))
+
+    updater.start_polling()
+
+    updater.idle()
+
+    scheduler_thread.join()
